c490945e2401b7f1cde2f422bb311da4fd2c4b11
ferdoushi <62441835+FerdoushiNahar@users.noreply.github.com>
2025-05-02 12:34:21 +0200

    Portfolio Updated

---
+++ src/main/resources/static/js/script.js
@@ -0,0 +1,7 @@
+/*=================typing animation============*/
+var typed = new Typed(".typing",{
+  strings:["","web Designer",'Web Developer',"Java Developer","Data Scientist"],
+  typeSpeed:100,
+  BackSpeed:60,
+  loop:true
+})
